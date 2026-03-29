ab10351e7497a6c03ceee1a1225d27c598faf087
divyanshi-adhikari <divyanshiadhikari11@gmail.com>
2026-03-29 23:22:23 +0530

    Added ML model using Decision Tree

--- src/model.py
+++ src/model.py
@@ -1,1 +1,17 @@
-print("Digital Addiction Analyzer Project Started")
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+# Load dataset
+data = pd.read_csv('../data/dataset.csv')
+
+# Features & labels
+X = data[['screen_time', 'social_media_hours', 'sleep_hours', 'study_hours']]
+y = data['addiction_level']
+
+# Train model
+model = DecisionTreeClassifier()
+model.fit(X, y)
+
+# Prediction function
+def predict_addiction(input_data):
+    return model.predict([input_data])[0]
